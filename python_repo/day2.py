ec179e3649f9f774b7942055116b98307c73aa21
CoderCraftsman9569 <my7186695@gmail.com>
2024-12-25 00:33:48 +0530

    m

---
+++ python_repo/day2.py
@@ -0,0 +1,9 @@
+"""this want say that if you want to print something you can use print() function
+print() function is used to print the output on the screen
+
+print() function can take any number of arguments
+"""
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)

c69c84707033e0ff94efd6c4b9197ef05f9b1174
CoderCraftsman9569 <my7186695@gmail.com>
2024-12-25 00:21:11 +0530

    change

---
+++ python_repo/day1.py
@@ -0,0 +1,6 @@
+#this is a comment in python
+#and this my first day python programming
+#single coment in python written using #
+#multiline comment written in python using ''' ''' or """ """(doble or triple quots )
+print("Hello World")
+print(5)
